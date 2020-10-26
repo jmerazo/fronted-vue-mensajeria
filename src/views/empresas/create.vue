ddff0e42c0716c676b2f9444610d08a59d89721a
Yerson Muñoz Erazo <jmerazo96@gmail.com>
2020-10-25 22:56:22 -0500

    fix error

--- src/views/empresas/create.vue
+++ src/views/empresas/create.vue
@@ -6,22 +6,16 @@
     <div>
       <label for="nombre">Nombre</label>
       <input type="text" v-model="nombre" id="nombre"/><br>
-
       <label for="razon_social">Razon Social</label>
       <input type="text" v-model="razon_social" id="razon_social"/><br>
-
       <label for="nit">Nit</label>
       <input type="text" v-model="nit" id="nit"/><br>
-
       <label for="direccion_empresa">Dirección</label>
       <input type="text" v-model="direccion_empresa" id="direccion_empresa"/><br>
-
       <label for="ciudad">Ciudad</label>
       <input type="text" v-model="ciudad" id="ciudad" /><br>
-
       <label for="departamento">Departamento</label>
       <input type="text" v-model="departamento" id="departamento" /><br>
-
       <label for="logo">Logo</label>
       <button type="submit">Seleccionar archivo</button>
     </div>
@@ -31,11 +25,18 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "CrearEmpresa",
   data() {
     return {
       nombre: "",
+      razon_social: "",
+      nit: "",
+      direccion: "",
+      ciudad: "",
+      departamento: "",
+      logo: "",
     };
   },
   methods: {
