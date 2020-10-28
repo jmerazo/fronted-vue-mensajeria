1e36f8617b51cc9018fbd0157af8adc7ff73699d
Yerson Muñoz Erazo <jmerazo96@gmail.com>
2020-10-28 08:11:54 -0500

    Mod empresas

--- src/views/empresas/create.vue
+++ src/views/empresas/create.vue
@@ -2,25 +2,36 @@
   <h1>Crear empresa</h1>
   <a href="/empresas">Volver</a>
 
-  <form @submit.prevent="guardar">
+  <form @submit.prevent="guardar" enctype="multipart/form-data">
     <div>
       <label for="nombre">Nombre</label>
-      <input type="text" v-model="nombre" id="nombre" /><br />
+      <input type="text" v-model="empresa.nombre" id="nombre" /><br />
       <label for="razon_social">Razon Social</label>
-      <input type="text" v-model="razon_social" id="razon_social" /><br />
+      <input type="text" v-model="empresa.razon_social" id="razon_social" /><br />
       <label for="nit">Nit</label>
-      <input type="text" v-model="nit" id="nit" /><br />
+      <input type="text" v-model="empresa.nit" id="nit" /><br />
       <label for="direccion_empresa">Dirección</label>
-      <input type="text" v-model="direccion_empresa" id="direccion_empresa" /><br/>
+      <input
+        type="text"
+        v-model="empresa.direccion_empresa"
+        id="direccion_empresa"
+      /><br />
       <label for="ciudad">Ciudad</label>
-      <input type="text" v-model="ciudad_empresa" id="ciudad_empresa" /><br />
+      <input type="text" v-model="empresa.ciudad_empresa" id="ciudad_empresa" /><br />
       <label for="departamento">Departamento</label>
-      <input type="text" v-model="departamento_empresa" id="departamento_empresa" /><br />
+      <input
+        type="text"
+        v-model="empresa.departamento_empresa"
+        id="departamento_empresa"
+      /><br />
       <label for="logo">Logo</label>
       <!-- <input type="text" v-model="logo" id="logo" /><br /> -->
       <!-- <input type="file" accept="image/*" @change="uploadImage($event)" id="logo"/> -->
-      <input type="file" v-on:change="logo" accept="image/*" id="logo"/>
+      <input type="file" @change="obtenerImagen" id="logo" />
       <!-- <button type="submit">Seleccionar archivo</button> -->
+      <figure>
+        <img width="150" height="150" :src="logo" />
+      </figure>
     </div>
 
     <button type="submit">Guardar</button>
@@ -34,66 +45,56 @@ export default {
   name: "CrearEmpresa",
   data() {
     return {
-      nombre: "",
-      razon_social: "",
-      nit: "",
-      direccion_empresa: "",
-      ciudad_empresa: "",
-      departamento_empresa: "",
-      logo: "",
+      logoMiniatura: "",
+      empresa: {
+        nombre: "",
+        razon_social: "",
+        nit: "",
+        direccion_empresa: "",
+        ciudad_empresa: "",
+        departamento_empresa: "",
+        logo: "",
+      },
     };
   },
   methods: {
-    guardar() {
-
-      let logo = new FormData();
-      logo.append("name", "my-picture");
-      logo.append("file", event.target.files);
-
-      let config = {
-        header: {
-          "Content-Type": "multipart/form-data",
-        },
+    obtenerImagen(e) {
+      let file = e.target.files[0];
+      this.empresa.logo = file;
+      this.cargarImagen(file);
+    },
+    cargarImagen(file) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.logoMiniatura = e.target.result;
       };
+      reader.readAsDataURL(file);
+    },
+    guardar() {
+      let data = new FormData();
+      data.append("nombre", this.empresa.nombre);
+      data.append("razon_social", this.empresa.razon_social);
+      data.append("nit", this.empresa.nit);
+      data.append("direccion_empresa", this.empresa.direccion_empresa);
+      data.append("ciudad_empresa", this.empresa.ciudad_empresa);
+      data.append("departamento_empresa", this.empresa.departamento_empresa);
+      data.append("logo", this.empresa.logo);
 
-      axios.post("http://localhost:8000/api/empresa/", {
-            nombre: this.nombre,
-            razon_social: this.razon_social,
-            nit: this.nit,
-            direccion_empresa: this.direccion_empresa,
-            ciudad_empresa: this.ciudad,
-            departamento_empresa: this.departamento, 
-            logo: this.logo,           
+      axios
+        .post("http://localhost:8000/api/empresa/", data, {
+          headers: {
+            Authorization: "JWT " + localStorage.getItem("token"),
           },
-          {
-            headers: {
-              Authorization: "JWT " + localStorage.getItem("token"),
-            },
-          }
-        )
+        })
         .then((response) => {
           this.$router.push("/empresas");
         });
     },
-    /* uploadImage(event) {
-      //const URL = "http://foobar.com/upload";
-
-      let data = new FormData();
-      data.append("name", "my-picture");
-      data.append("file", event.target.files[0]);
-
-      let config = {
-        header: {
-          "Content-Type": "image/png",
-        },
-      };
-
-      axios.post("http://localhost:8000/api/empresa/", data, config, {
-          logo: this.logo,
-      }).then((response) => {
-        console.log("Imagen cargada")
-      });
-    }, */
+  },
+  computed: {
+    logo() {
+      return this.logoMiniatura;
+    },
   },
 };
 </script>
