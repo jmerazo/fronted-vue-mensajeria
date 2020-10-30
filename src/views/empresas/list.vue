9c470f1c0aefed2a966a3a6659053d2776f4ad08
Yerson Muñoz Erazo <jmerazo96@gmail.com>
2020-10-30 08:17:55 -0500

    Bootstrap

--- src/views/empresas/list.vue
+++ src/views/empresas/list.vue
@@ -1,7 +1,8 @@
 <template>
-  <h1>Listado de empresas</h1>
-  <a href="/empresas/create">Crear</a>
-  <table>
+  <h1 align="center">Empresas</h1>
+  <a href="/empresas/create">Agregar</a>
+  <a href="/">Inicio</a>
+  <table class="table table-striped" id="table">
     <thead>
       <tr>
         <td>ID</td>
@@ -9,15 +10,24 @@
         <td>Razon Social</td>
         <td>Nit</td>
         <td>Dirección</td>
+        <td>Ciudad</td>
+        <td>Departamento</td>
+        <td>Acción</td>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="empresa in empresas" :key="empresa.id">
+      <tr v-for="(empresa, index) in empresas" :key="empresa.id">
         <td>{{ empresa.id }}</td>
         <td>{{ empresa.nombre }}</td>
         <td>{{ empresa.razon_social }}</td>
         <td>{{ empresa.nit }}</td>
         <td>{{ empresa.direccion_empresa }}</td>
+        <td>{{ empresa.ciudad_empresa }}</td>
+        <td>{{ empresa.departamento_empresa }}</td>
+        <div>
+          <button class="btn btn-success"><router-link to="/empresas/update"> ✏️ </router-link></button>
+          <button class="btn btn-danger" @click="eliminar_empresa(index, item)"> 🗑️ </button>
+        </div>
       </tr>
     </tbody>
   </table>
@@ -47,5 +57,33 @@ export default {
               this.error = true;
     }); */
   },
+  methods: {
+    eliminar_empresa(index, item) {
+      console.log(index);
+      console.log(item);
+      this.empresas.splice(index, 1);
+
+      let data = new FormData();
+      data.append("id", item.id);
+
+      axios
+        .delete("http://localhost:8000/api/empresa/", data, {
+          headers: {
+            Authorization: "JWT " + localStorage.getItem("token"),
+          },
+        })
+        .then((response) => {
+          this.$router.push("/empresas");
+        });
+    }
+  }
 };
 </script>
+
+<style>
+
+* {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+</style>
